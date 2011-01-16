d7772f4ec1b9866c8dc1cd14f8db0782412d0238
Frucot Jean-Louis <frucot.jeanlouis@free.fr>
2011-01-16 17:17:44 +0100

    Mise à jour de l'aide fix #1095 Correction du nom de l'appli Debianisation

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include "abuleduaproposv0.h"
+
+//-------------------Nom visible de l'application---------------------
+//
+extern const QString abeApplicationLongName="Leterrier d'AbulEdu - Nombre Cible";
+//
+//--------------------------------------------------------------------
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    qApp->setApplicationName("NombreCible");
-    qApp->setApplicationVersion("v1");
-    qApp->setOrganizationDomain("http://entraide.abuledu-fr.org/rss/topic/nombre-cible");
+    qApp->setApplicationName("leterrier-cibler");
+    qApp->setApplicationVersion("1.0.0");
+    qApp->setOrganizationDomain("abuledu.org");
+    qApp->setOrganizationName("leterrier");
 
     MainWindow w;
     w.setFixedSize(600, 400);
