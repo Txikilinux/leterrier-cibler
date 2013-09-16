c8d1ea71a6264972df55b986ac1d68c1c3c6a45c
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2013-09-16 16:34:48 +0200

    Passage du numéro de version en 2.0

--- version.h
+++ version.h
@@ -1,11 +1,34 @@
+/**
+  * Cibler (le Terrier d'AbulÉdu)
+  *
+  * @warning aucun traitement d'erreur n'est pour l'instant implémenté
+  * @see https://redmine.ryxeo.com/projects/leterrier-cibler
+  * @author 2011-2013 Eric Seigne <eric.seigne@ryxeo.com>
+  * @see The GNU Public License (GPL)
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation; either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful, but
+  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+  * for more details.
+  *
+  * You should have received a copy of the GNU General Public License along
+  * with this program; if not, write to the Free Software Foundation, Inc.,
+  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+  */
+
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,4,0
-#define VER_FILEVERSION_STR         "1.4.0\0"
+#define VER_FILEVERSION             2,0,0
+#define VER_FILEVERSION_STR         "2.0.0\0"
 
-#define VER_PRODUCTVERSION          1,4,0
-#define VER_PRODUCTVERSION_STR      "1.4.0\0"
+#define VER_PRODUCTVERSION          2,0,0
+#define VER_PRODUCTVERSION_STR      "2.0.0\0"
 
 #define VER_COMPANYNAME_STR         "RyXeo SARL"
 #define VER_FILEDESCRIPTION_STR     "AbulEdu LeTerrier -- Le nombre cible"
