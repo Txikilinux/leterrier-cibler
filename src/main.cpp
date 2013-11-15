c5e848847a0208dd0336ef40f8134bf12056c319
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2013-11-15 12:42:32 +0100

    Margin top du bouton Avatar + applicationLongName

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,8 @@
 
 int main(int argc, char *argv[])
 {
-    AbulEduApplicationV1 a(argc, argv,VER_INTERNALNAME_STR, VER_PRODUCTVERSION_STR, VER_COMPANYDOMAIN_STR, "leterrier");
-    a.setAbeApplicationLongName(QObject::trUtf8(VER_FILEDESCRIPTION_STR));
+    AbulEduApplicationV1 a(argc, argv,VER_INTERNALNAME_STR, VER_PRODUCTVERSION_STR, VER_COMPANYDOMAIN_STR, VER_COMPANYNAME_STR);
+    a.setAbeApplicationLongName(QObject::trUtf8(VER_PRODUCTNAME_STR));
 
     MainWindow w;
 #if defined(Q_OS_ANDROID)
